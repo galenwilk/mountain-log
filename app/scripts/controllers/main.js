0664a185ac3bacc8d91b874f902f96e70e0a79a8
galenwilk <galen@leaf.me>
2014-08-17 16:56:02 -0400

    start entries layout.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,16 @@
  */
 angular.module('mountainLogApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.entries = [
+      {
+        date: '2014-04-05',
+        location: 'Vail, CO',
+        id: '100'
+      },
+      {
+        date: '2014-01-26',
+        location: 'Lake Tahoe, NV',
+        id: '101'
+      }
     ];
   });
